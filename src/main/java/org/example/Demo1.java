a2a8e0bbfa72b3947c8826f715c2c057a83e0635
admin <sugatenitesh@gmail.com>
2024-11-30 13:31:40 +0530

    Demo1 class to New branch

---
+++ src/main/java/org/example/Demo1.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Demo1 {
+    public static void bike(){
+        System.out.println("Hello Bike");
+
+    }
+    public static void main(String[] args) {
+        Demo abc = new Demo();
+        abc.car();
+        bike();
+    }
+}
