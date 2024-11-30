66ed4fc475b7e2b2e2e2d2550be77c1fbb8daebb
admin <sugatenitesh@gmail.com>
2024-11-30 13:06:14 +0530

    Demo class

---
+++ src/main/java/org/example/Demo.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Demo {
+    public void car(){
+        System.out.println("hello car");
+    }
+
+    public static void main(String[] args) {
+        Demo ab = new Demo();
+        ab.car();
+    }
+}
