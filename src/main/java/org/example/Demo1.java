d7a89b829363290d311c5a4ac055f8d8dfc35bc3
admin <sugatenitesh@gmail.com>
2024-11-30 14:54:17 +0530

    comments added

--- src/main/java/org/example/Demo1.java
+++ src/main/java/org/example/Demo1.java
@@ -3,7 +3,8 @@ package org.example;
 public class Demo1 {
     public static void bike(){
         System.out.println("Hello Bike123");
-
+    //made some changes
+        //two lines added
     }
     public static void main(String[] args) {
         Demo abc = new Demo();
