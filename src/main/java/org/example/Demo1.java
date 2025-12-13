97e80d4f223f7de94c72135c4e9983ae7cf203fa
admin <sugatenitesh@gmail.com>
2025-12-13 17:00:02 +0530

    Demo1 class Created

---
+++ src/main/java/org/example/Demo1.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Demo1 {
+
+    public  void ABC(){
+        System.out.println("Hello Method ABC");
+    }
+
+    public  void AV(){
+        System.out.println("Hello Method AV");
+    }
+
+    public static void main(String[] args) {
+        Demo1 b = new Demo1();
+        b.ABC();
+        b.AV();
+    }
+}
